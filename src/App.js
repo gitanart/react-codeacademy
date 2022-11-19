7ec3d7f16cb8ffe824b89ced379d9215138068ea
admin <admin@admins-MacBook-Air.local>
2022-11-19 12:09:17 +0200

    paskaita 6 pirma dalis

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import {Counter} from './Counter/Counter';
 
 function App() {
-  const [isCounterVisible, setIsCounterVisible] = useState(true);
+  const [count, setCount] = useState(0);
 
-  const handleToggCounter = () => {
-    setIsCounterVisible(!isCounterVisible);
+  useEffect(() => {
+     console.log('COMPONENT MOUNTED');
+  }, []);
+
+  const handleIncrement = (event) => {
+    setCount(count + 1);
+  }
+
+  const handleIncrementKeyDown = (event) => {
+    console.log('KEY DOWN');
+  }
+
+  const handleIncrementKeyUp = (event) => {
+    console.log('KEY UP');
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('SUBMIT');
+  }
+
+  const handleInputBlur = () => {
+    console.log('INPUT LOST FOCUS');
   }
 
   return (
     <div className="App">
-      <button onClick={handleToggCounter}>Toggle Counter</button>
-       {isCounterVisible && <Counter />}
+      <form onSubmit={handleSubmit}>
+        {count}
+        <button 
+          onKeyUp={handleIncrementKeyUp} 
+          onKeyDown={handleIncrementKeyDown} 
+          onClick={handleIncrement}
+        >
+          Increment
+        </button>
+         <input onChange={handleInputChange} onBlur={handleInputBlur}/>
+      </form>
     </div>
   );
 }
