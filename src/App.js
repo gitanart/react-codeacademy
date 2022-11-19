5e6dd52c556bc77445b5b41ae66bccfab4a4e0ae
admin <admin@admins-MacBook-Air.local>
2022-11-19 16:28:23 +0200

    paskaita 6 antra dalis

--- src/App.js
+++ src/App.js
@@ -2,50 +2,34 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [searchInputValue, setSearchInputValue] = useState('');
 
   useEffect(() => {
-     console.log('COMPONENT MOUNTED');
+     fetch('https://jsonplaceholder.typicode.com/posts')
+     .then((res) => res.json())
+     .then((data) => {
+       setPosts(data);
+     });
   }, []);
 
-  const handleIncrement = (event) => {
-    setCount(count + 1);
-  }
-
-  const handleIncrementKeyDown = (event) => {
-    console.log('KEY DOWN');
-  }
-
-  const handleIncrementKeyUp = (event) => {
-    console.log('KEY UP');
-  }
-
-  const handleInputChange = (event) => {
-    console.log(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('SUBMIT');
-  }
-
-  const handleInputBlur = () => {
-    console.log('INPUT LOST FOCUS');
-  }
-
+  const handleInputChange = (event) => setSearchInputValue(event.target.value);
+  
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        {count}
-        <button 
-          onKeyUp={handleIncrementKeyUp} 
-          onKeyDown={handleIncrementKeyDown} 
-          onClick={handleIncrement}
-        >
-          Increment
-        </button>
-         <input onChange={handleInputChange} onBlur={handleInputBlur}/>
-      </form>
+      <input onChange={handleInputChange} />
+      {
+        posts
+          .filter((post) => {
+            return post.title.indexOf(searchInputValue) >= 0
+          })
+          .map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+           </div>
+          ))
+      }
     </div>
   );
 }
